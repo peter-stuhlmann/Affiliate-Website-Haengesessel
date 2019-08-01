0d0fda3976de45aa02a3680cc12fdaf5bb8c95a3
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-08-01 20:41:26 +0200

    Create React App

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import '../assets/scss/main.scss';
+
+export default class App extends Component {
+
+  render() {
+    return ( 
+      <Fragment>
+        Hello World!
+      </Fragment>
+    )
+  }
+
+}

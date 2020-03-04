b5792cfc3714b00d3d6da229e6f5f79aa55f25ef
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-04 23:28:28 +0100

    Move Router in a separate file

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyles } from './StyledComponents';
 import HeaderComponent from './Header';
 import MainNavigation from './MainNavigation';
-import StaticPage from './StaticPage';
-import NotFound from './NotFound';
 import FooterComponent from './Footer';
 import ContextProvider from '../Context';
+import Router from './Router';
 
 export default function App() {
   return (
     <ContextProvider>
       <BrowserRouter>
         <GlobalStyles />
-
         <HeaderComponent />
         <MainNavigation />
-
-        <Switch>
-          <Route exact path="/" component={StaticPage} />
-          <Route exact path="/datenschutzerklaerung" component={StaticPage} />
-          <Route exact path="/impressum" component={StaticPage} />
-          <Route component={NotFound} />
-        </Switch>
-
+        <Router />
         <FooterComponent />
       </BrowserRouter>
     </ContextProvider>
---
+++ src/components/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import StaticPage from './StaticPage';
+import NotFound from './NotFound';
+
+export default function Router() {
+  return (
+    <Switch>
+      <Route exact path="/" component={StaticPage} />
+      <Route exact path="/datenschutzerklaerung" component={StaticPage} />
+      <Route exact path="/impressum" component={StaticPage} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}

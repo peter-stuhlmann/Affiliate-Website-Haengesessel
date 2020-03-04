c549bba89fab9a75c3632f4a86b7dc8c7511b0a0
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-04 15:05:25 +0100

    Use Context to render text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../Context';
 import { Header, Wrapper, Logo } from './StyledComponents';
 
 export default function HeaderComponent() {
+  const { text } = useContext(Context);
+
   return (
     <Header>
       <Wrapper>
         <Logo>
           <Link to="/">
-            <p className="site-title">Lorem ipsum</p>
+            <p className="site-title">{text.siteTitle}</p>
           </Link>
-          <p className="site-description">Lorem ipsum dolor sit amet</p>
+          <p className="site-description">{text.siteDescription}</p>
         </Logo>
       </Wrapper>
     </Header>

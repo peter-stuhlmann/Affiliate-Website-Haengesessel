08206040f0b8420f333b4dddc886d7c119509b0d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-04 23:37:04 +0100

    Create a component for linked images

---
+++ src/components/Image.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const LinkedImage = props => {
+  const { alt, caption, href, source, src, title, width } = props;
+
+  return (
+    <StyledImage>
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <img src={src} title={title} alt={alt} style={{ maxWidth: width }} />
+      </a>
+      <p>
+        {caption}
+        <span>{source}</span>
+      </p>
+    </StyledImage>
+  );
+};
+
+const StyledImage = styled.div`
+  img {
+    width: 100%;
+  }
+
+  span {
+    font-size: 0.8em;
+    font-style: italic;
+    margin-left: 5px;
+  }
+`;

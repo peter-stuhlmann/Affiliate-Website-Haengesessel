1fbd0abffb37d5815d14e44c7609f858f3612c34
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-05 20:23:45 +0100

    Repair NotFound page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,16 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../../Context';
 import { Wrapper } from '../Wrapper';
+import { meta } from '../../helper/meta';
 
 export default function NotFound() {
   const { text } = useContext(Context);
+
+  useEffect(() => {
+    document.title = text.home.meta.title;
+  }, [text.home.meta.title]);
+
+  meta(text.home.meta.description);
 
   return (
     <Wrapper>
       <div
         dangerouslySetInnerHTML={{
-          __html: text.notFound,
+          __html: text.notFound.content,
         }}
       />
       <Link to="/">Home</Link>

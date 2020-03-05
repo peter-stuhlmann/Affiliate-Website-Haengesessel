396b262e58ccf0fb826a4b5265ee7b8b61e9a80f
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-05 16:52:53 +0100

    Add a new route

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Context } from '../Context';
 import Home from './pages/Home';
 import StaticPage from './pages/StaticPage';
 import NotFound from './pages/NotFound';
+import { ShareButtons } from './Share';
 
-export default function Router() {
+export default function Router(props) {
+  const { text } = useContext(Context);
+
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/datenschutzerklaerung" component={StaticPage} />
-      <Route exact path="/impressum" component={StaticPage} />
+      <Route
+        exact
+        path="/datenschutzerklaerung"
+        render={() => <StaticPage content={text.privacyPolicy} />}
+      />
+      <Route
+        exact
+        path="/impressum"
+        render={() => <StaticPage content={text.legalNotice} />}
+      />
+      <Route
+        exact
+        path="/kaufberatung"
+        render={() => (
+          <Fragment>
+            <StaticPage content={text.buyingAdvice} />
+            <ShareButtons />
+          </Fragment>
+        )}
+      />
       <Route component={NotFound} />
     </Switch>
   );

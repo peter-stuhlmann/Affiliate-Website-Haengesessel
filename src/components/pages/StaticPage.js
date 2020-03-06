a7b885fa4ab8afa8248a1d7cdb07bb6fa05cfc0a
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-06 01:05:50 +0100

    Use switch statement to render content depending on pathname

--- src/components/pages/StaticPage.js
+++ src/components/pages/StaticPage.js
@@ -1,9 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import { Wrapper } from '../Wrapper';
+import { Context } from '../../Context';
 import { meta } from '../../helper/meta';
+import { ShareButtons } from '../Share';
 
-export default function StaticPage(props) {
-  const { content, description, title } = props;
+export default function StaticPage() {
+  const { text } = useContext(Context);
+
+  let content, description, share, title;
+
+  switch (document.location.pathname) {
+    case '/datenschutzerklaerung':
+      content = text.privacyPolicy.content;
+      description = text.privacyPolicy.meta.description;
+      title = text.privacyPolicy.meta.title;
+      share = false;
+      break;
+    case '/impressum':
+      content = text.legalNotice.content;
+      description = text.legalNotice.meta.description;
+      title = text.legalNotice.meta.title;
+      share = false;
+      break;
+    case '/kaufberatung':
+      content = text.buyingAdvice.content;
+      description = text.buyingAdvice.meta.description;
+      title = text.buyingAdvice.meta.title;
+      share = true;
+      break;
+    default: // ...
+  }
 
   useEffect(() => {
     document.title = title;
@@ -12,12 +38,13 @@ export default function StaticPage(props) {
   meta(description);
 
   return (
-    <>
+    <Fragment>
       <Wrapper
         dangerouslySetInnerHTML={{
           __html: content,
         }}
       />
-    </>
+      {share ? <ShareButtons /> : ''}
+    </Fragment>
   );
 }

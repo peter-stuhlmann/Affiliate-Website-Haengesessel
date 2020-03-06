bc50e036b3dd5606a94be0e6e420bac82083bda7
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-06 01:09:20 +0100

    Integrate GoogleAnalytics tracking

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,51 +4,16 @@ import { Context } from '../Context';
 import Home from './pages/Home';
 import StaticPage from './pages/StaticPage';
 import NotFound from './pages/NotFound';
-import { ShareButtons } from './Share';
+import { ga } from '../helper/analytics';
 
 export default function Router() {
-  const { text } = useContext(Context);
-
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route
-        exact
-        path="/datenschutzerklaerung"
-        render={() => (
-          <StaticPage
-            content={text.privacyPolicy.content}
-            title={text.privacyPolicy.meta.title}
-            description={text.privacyPolicy.meta.decription}
-          />
-        )}
-      />
-      <Route
-        exact
-        path="/impressum"
-        render={() => (
-          <StaticPage
-            content={text.legalNotice.content}
-            title={text.legalNotice.meta.title}
-            description={text.legalNotice.meta.decription}
-          />
-        )}
-      />
+      <Route exact path="/" component={ga(Home)} />
+      <Route exact path="/datenschutzerklaerung" component={ga(StaticPage)} />
+      <Route exact path="/impressum" component={ga(StaticPage)} />
       <Route exact path="/haengesessel" render={() => <Redirect to="/" />} />
-      <Route
-        exact
-        path="/kaufberatung"
-        render={() => (
-          <Fragment>
-            <StaticPage
-              content={text.buyingAdvice.content}
-              title={text.buyingAdvice.meta.title}
-              description={text.buyingAdvice.meta.decription}
-            />
-            <ShareButtons />
-          </Fragment>
-        )}
-      />
+      <Route exact path="/kaufberatung" component={ga(StaticPage)} />
       <Route component={NotFound} />
     </Switch>
   );

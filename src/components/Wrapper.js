1fdc58b39b1570cb1e731de6054abbf39379459a
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-04-18 16:58:55 +0200

    Update Wrapper styling

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Wrapper = props => {
+export const Wrapper = (props) => {
   const {
     backgroundColor,
+    boxSizing,
     children,
     color,
     dangerouslySetInnerHTML,
+    display,
+    flexFlow,
+    jusifyContent,
+    lineHeight,
     marginX,
     maxWidth,
     paddingX,
@@ -16,8 +21,13 @@ export const Wrapper = props => {
   return (
     <StyledWrapper
       backgroundColor={backgroundColor}
+      boxSizing={boxSizing}
       color={color}
       dangerouslySetInnerHTML={dangerouslySetInnerHTML}
+      display={display}
+      flexFlow={flexFlow}
+      jusifyContent={jusifyContent}
+      lineHeight={lineHeight}
       marginX={marginX}
       maxWidth={maxWidth}
       paddingX={paddingX}
@@ -29,11 +39,15 @@ export const Wrapper = props => {
 };
 
 const StyledWrapper = styled.div`
-  background-color: ${props => props.backgroundColor || '#fff'};
-  box-sizing: border-box;
-  color: ${props => props.color || '#000'};
-  margin: ${props => props.marginX || '30px auto'};
-  max-width: ${props => props.maxWidth || '1170px'};
-  padding: ${props => props.paddingX || '15px'};
-  width: ${props => props.width || '100%'};
+  background-color: ${(props) => props.backgroundColor || '#fff'};
+  box-sizing: ${(props) => props.boxSizing || 'border-box'};
+  color: ${(props) => props.color || '#505050'};
+  display: ${(props) => props.display || 'flex'};
+  flex-flow: ${(props) => props.flexFlow || 'row wrap'};
+  justify-content: ${(props) => props.jusifyContent || 'space-between'};
+  line-height: ${(props) => props.lineHeight || '1.6'};
+  margin: ${(props) => props.marginX || '30px auto'};
+  max-width: ${(props) => props.maxWidth || '1170px'};
+  padding: ${(props) => props.paddingX || '15px'};
+  width: ${(props) => props.width || '100%'};
 `;

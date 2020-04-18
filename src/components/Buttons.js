428e7d1acc7aff21004766c269015816b075c48e
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-04-18 18:51:00 +0200

    Create a button component

---
+++ src/components/Buttons.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Button = (props) => {
+  const {
+    backgroundColor,
+    backgroundColorHover,
+    color,
+    href,
+    linkText,
+    maxWidth,
+    width,
+  } = props;
+
+  return (
+    <StyledButton
+      backgroundColor={backgroundColor}
+      backgroundColorHover={backgroundColorHover}
+      color={color}
+      href={href}
+      maxWidth={maxWidth}
+      width={width}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {linkText}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.a`
+  background-color: ${(props) => props.backgroundColor || '#9c4808'};
+  border-radius: 5px;
+  box-sizing: border-box;
+  color: ${(props) => props.color || '#fff'};
+  display: block;
+  line-height: 1.6;
+  margin: 20px auto 15px auto;
+  max-width: ${(props) => props.maxWidth || '300px'};
+  padding: 15px;
+  text-align: center;
+  text-decoration: none;
+  transition: 0.2s;
+  width: ${(props) => props.width || '100%'};
+
+  &:hover {
+    background-color: ${(props) => props.backgroundColorHover || '#431f04'};
+  }
+`;

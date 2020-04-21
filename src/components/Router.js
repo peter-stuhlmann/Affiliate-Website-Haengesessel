20fe280ce7a24c528c6ce36abb949c48f9598557
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-04-21 23:37:55 +0200

    Load route components with lazy loading

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Home from './pages/Home';
-import Equipment from './pages/Equipment';
-import StaticPage from './pages/StaticPage';
-import LegalNotice from './pages/LegalNotice';
-import Contact from './pages/Contact';
-import BuyingAdvice from './pages/BuyingAdvice';
-import NotFound from './pages/NotFound';
+
 import { ga } from '../helper/analytics';
+import { Wrapper } from './Wrapper';
+
+const Home = lazy(() => import('./pages/Home'));
+const BuyingAdvice = lazy(() => import('./pages/BuyingAdvice'));
+const Equipment = lazy(() => import('./pages/Equipment'));
+const Contact = lazy(() => import('./pages/Contact'));
+const LegalNotice = lazy(() => import('./pages/LegalNotice'));
+const StaticPage = lazy(() => import('./pages/StaticPage'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 export default function Router() {
   return (
-    <Switch>
-      <Route exact path="/" component={ga(Home)} />
-      <Route exact path="/datenschutzerklaerung" component={ga(StaticPage)} />
-      <Route exact path="/zubehoer" component={ga(Equipment)} />
-      <Route exact path="/impressum" component={ga(LegalNotice)} />
-      <Route exact path="/haengesessel" render={() => <Redirect to="/" />} />
-      <Route exact path="/kaufberatung" component={ga(BuyingAdvice)} />
-      <Route exact path="/kontakt" component={ga(Contact)} />
-      <Route component={ga(NotFound)} />
-    </Switch>
+    <Suspense fallback={<Wrapper>Inhalt wird geladen...</Wrapper>}>
+      <Switch>
+        <Route exact path="/" component={ga(Home)} />
+        <Route exact path="/datenschutzerklaerung" component={ga(StaticPage)} />
+        <Route exact path="/zubehoer" component={ga(Equipment)} />
+        <Route exact path="/impressum" component={ga(LegalNotice)} />
+        <Route exact path="/haengesessel" render={() => <Redirect to="/" />} />
+        <Route exact path="/kaufberatung" component={ga(BuyingAdvice)} />
+        <Route exact path="/kontakt" component={ga(Contact)} />
+        <Route component={ga(NotFound)} />
+      </Switch>
+    </Suspense>
   );
 }

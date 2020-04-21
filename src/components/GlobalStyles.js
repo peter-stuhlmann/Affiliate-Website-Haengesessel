8a9797a3ec2a3aaf466fa0e571d4eb5ec0fe079d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-04-21 23:21:52 +0200

    Change h/h1 styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -29,6 +29,11 @@ export const GlobalStyles = createGlobalStyle`
 
     h1, h2, h3, h4, h5, h6 {
       font-family: 'Viga', sans-serif;
+      font-weight: normal;
+    }
+
+    h1 {
+      font-size: 2em;
     }
   }
 `;
